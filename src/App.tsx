cf4a4503b3ab2263c78c8c6e334d34a81c913917
Rehan Maulana <rehanmaul111@gmail.com>
2023-06-19 16:15:21 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex text-center justify-center max-w-max border border-red-100 h-screen">
+      halo gaies
+    </div>
+  );
+}
+
+export default App;
