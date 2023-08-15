3eff6fae4b6339adef4677669b6b088f08f99149
Rehan Maulana <rehanmaul111@gmail.com>
2023-08-15 12:13:19 +0700

    refactor: change description

--- src/components/organism/EducationCard/constant.ts
+++ src/components/organism/EducationCard/constant.ts
@@ -8,12 +8,12 @@ export const educationCard: IEducationCard[] = [
     degree: "Bootcamp Front-End MSIB",
   },
   {
-    name: "Institute Technology and Business ASIA Malang",
+    name: "Institut Teknologi dan Bisnis ASIA Malang",
     duration: "2020 June - Present",
     degree: "S1 - Informatics Engineering",
   },
   {
-    name: "Vocational High School 8 Malang",
+    name: "SMKN 8 Malang",
     duration: "2017 June - 2020",
     degree: "Electronic Industry",
   },
